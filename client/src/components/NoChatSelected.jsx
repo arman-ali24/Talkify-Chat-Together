4fa4a24935fbf8ce28a6dc69da190cffb75539d5
Arman Ali <armanali0178614@gmail.com>
2026-02-14 18:40:49 +0530

    add chat section

--- client/src/components/NoChatSelected.jsx
+++ client/src/components/NoChatSelected.jsx
@@ -1,7 +1,14 @@
 import { MessageSquare } from "lucide-react";
 
 const NoChatSelected = () => {
-  return <></>;
+  return <>
+  
+  <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-white/50">
+    <div></div>
+  </div>
+  
+  
+  </>;
 };
 
 export default NoChatSelected;
